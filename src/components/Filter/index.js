0085d8774badb880162036404e355f61c7c47f87
Ivan Garcia <ivangm180@gmail.com>
2022-03-26 20:06:23 -0600

    finally finished the filter 😪

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,113 +1,41 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React, { useState } from "react";
+import React from "react";
 
-const spanStyle = {
-  position: "relative",
-};
-
-const filterStyles = {
-  background: "white",
-  left: "200px",
-};
-
-function Filter({ setFilms, films }) {
-  const [open, setOpen] = useState(false);
-  const [isAscendant, setIsAscendant] = useState(false);
-  const [runningTime, setIsRunningTime] = useState(false);
-  const [score, setScore] = useState(false);
-  const [isDate, setIsDate] = useState(false);
-
-  const handleRunningTime = () => {
-    if (runningTime) {
-      if (isAscendant) {
-        films.sort((a, b) => {
-          return b.running_time - a.running_time;
-        });
-        return films;
-      } else {
-        films.sort((a, b) => {
-          return a.running_time - b.running_time;
-        });
-        return films;
-      }
-    }
-    return null;
-  };
-
-  console.log(films);
-
+function Filter({ sortArray, setAdjust, adjust, categories, setCategories }) {
   return (
     <>
-      <span
-        style={spanStyle}
-        onClick={() => {
-          setOpen(!open);
-        }}
-      >
-        Filter
-      </span>
-      {!!open && (
-        <div>
-          <ul style={filterStyles}>
-            <li>
-              Filter: <span>Date</span>
-              <label htmlFor="">
-                <input
-                  type="checkbox"
-                  onChange={(e) => {
-                    let isChecked = e.target.checked;
-                    setIsDate(isChecked);
-                  }}
-                />
-              </label>
-            </li>
-            <li>
-              Filter: <span>Running Time</span>
-              <label htmlFor="">
-                <input
-                  type="checkbox"
-                  onChange={(e) => {
-                    let isChecked = e.target.checked;
-                    setIsRunningTime(isChecked);
-                  }}
-                />
-              </label>
-            </li>
-            <li>
-              Filter: <span>Score</span>
-              <label htmlFor="">
-                <input
-                  type="checkbox"
-                  onChange={(e) => {
-                    let isChecked = e.target.checked;
-                    setScore(isChecked);
-                  }}
-                />
-              </label>
-            </li>
-            <li>
-              Order: <span>Ascendant</span>
-              <label htmlFor="">
-                <input
-                  type="checkbox"
-                  onChange={(e) => {
-                    let isChecked = e.target.checked;
-                    setIsAscendant(isChecked);
-                  }}
-                />
-              </label>
-            </li>
-            <button
-              onClick={() => {
-                handleRunningTime;
-              }}
-            >
-              Send
-            </button>
-          </ul>
-        </div>
-      )}
+      <div>
+        <h4 style={{ margin: "4px" }}>Filter</h4>
+        <label
+          style={{ display: "flex", flexDirection: "column" }}
+          onClick={() => sortArray(categories, adjust)}
+        >
+          <select
+            onChange={(e) => {
+              setCategories(e.target.value);
+            }}
+          >
+            <option value="none" selected disabled hidden>
+              Select an Option
+            </option>
+            <option value="running_time">Duration</option>
+            <option value="rt_score">Rate</option>
+            <option value="release_date">Year</option>
+          </select>
+          <select
+            onChange={(e) => {
+              setAdjust(e.target.value);
+            }}
+          >
+            <option value="none" selected disabled hidden>
+              Select an Option
+            </option>
+            <option value="ascendant">Ascendant</option>
+            <option value="descendant">Descendant</option>
+          </select>
+        </label>
+      </div>
     </>
   );
 }

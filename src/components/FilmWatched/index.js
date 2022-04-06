83b5e2f6dec0c4d8ffe0ca6f151e4966d8d32eca
lilianarestrepotorres <lilianarestrepot@gmail.com>
2022-04-06 19:31:01 -0400

    Adding state to watched component

--- src/components/FilmWatched/index.js
+++ src/components/FilmWatched/index.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import { FaHeart } from 'react-icons/fa';
 import './FilmWatched.css'
 
 function FilmWatched(){
+    const [watched, setWatched] = useState(false);
+    const [hoverWatched, setHoverWatched] = useState(false);
+
     return(
         <div className="film-watched-component">
             <label>
                 <input
                     type="checkbox"
+                    name="watched"
+                    onClick={()=>setWatched(true)}
                 />
                 <FaHeart
                     className="heart"
                     size={20}
+                    color={((watched || hoverWatched) === true) ? "#C22F80" : "#e4e5e9"}
+                    onMouseEnter = {()=> setHoverWatched(true)}
+                    onMouseLeave = {() => setHoverWatched(false)}
                 />
             </label>
-            
         </div>
     );
 }
 
 export { FilmWatched }
+

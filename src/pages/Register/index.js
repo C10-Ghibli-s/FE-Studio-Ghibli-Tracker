4d3cf110ee7f2a7d5fd8f7ca3878047baf807a7b
Ivan Garcia <ivangm180@gmail.com>
2022-04-07 00:23:17 -0500

    [ADD] creating the register form

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -14,16 +14,51 @@ function Register() {
           <span className="hero-tracker">TRACKER</span>
         </figure>
         <Formik
-          initialValues={{ user: "", password: "" }}
+          initialValues={{
+            username: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+          }}
           validate={(values) => {
             let errors = {};
-            //User validation
-            if (!values.user) {
-              errors.user = "Please enter your username";
+            if (!values.email) {
+              //Email validation
+              errors.email = "Please enter your email";
+            } else if (
+              !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+                values.email
+              )
+            ) {
+              errors.email = "This is not a valid email";
             }
+            //User validation
+            if (!values.username) {
+              errors.username = "Please enter your username";
+            } else if (
+              !/^(?=.{4,12}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/.test(
+                values.username
+              )
+            ) {
+              errors.username =
+                "Username must contains: numbers, letters, 6 - 22 characters. Spaces are not allowed";
+            }
+            // Password validation
             if (!values.password) {
-              //Password validation
               errors.password = "Please enter your password";
+            } else if (
+              !/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(
+                values.password
+              )
+            ) {
+              errors.password =
+                "Password must have: 8 characters at least, a lowercase, an uppercase, a number, a symbol";
+            }
+            // Confirm password validation
+            if (!values.confirmPassword) {
+              errors.confirmPassword = "Confirm password required";
+            } else if (values.password !== values.confirmPassword) {
+              errors.confirmPassword = "Password not matched";
             }
             return errors;
           }}
@@ -38,16 +73,31 @@ function Register() {
           {({ errors }) => (
             <Form className="formulario">
               <div>
-                <label htmlFor="user">User</label>
+                <label htmlFor="email">Email</label>
                 <Field
-                  id="user"
-                  name="user"
+                  id="email"
+                  name="email"
+                  type="text"
+                  placeholder="yourmail@mail.com"
+                />
+                <ErrorMessage
+                  name="email"
+                  component={() => <div className="error">{errors.email}</div>}
+                />
+              </div>
+              <div>
+                <label htmlFor="username">Username</label>
+                <Field
+                  id="username"
+                  name="username"
                   type="text"
                   placeholder="username"
                 />
                 <ErrorMessage
-                  name="user"
-                  component={() => <div className="error">{errors.user}</div>}
+                  name="username"
+                  component={() => (
+                    <div className="error">{errors.username}</div>
+                  )}
                 />
               </div>
               <div>
@@ -65,7 +115,22 @@ function Register() {
                   )}
                 />
               </div>
-              <button type="submit">Login</button>
+              <div>
+                <label htmlFor="confirmPassword">Confirm password</label>
+                <Field
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="confirm password"
+                />
+                <ErrorMessage
+                  name="confirmPassword"
+                  component={() => (
+                    <div className="error">{errors.confirmPassword}</div>
+                  )}
+                />
+              </div>
+              <button type="submit">Register</button>
               {/* <Facebook /> */}
             </Form>
           )}

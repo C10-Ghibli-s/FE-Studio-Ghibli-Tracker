a4a26fd1bde54fe5a5ea292541538cbda97b7fb3
lilianarestrepotorres <lilianarestrepot@gmail.com>
2022-04-06 21:49:53 -0400

    adding hover functionality to the component

--- src/components/FilmWatched/index.js
+++ src/components/FilmWatched/index.js
@@ -2,11 +2,17 @@ import React, {useState} from "react";
 import { FaHeart } from 'react-icons/fa';
 import './FilmWatched.css'
 
+//we should receive props with the value watched true or false
 function FilmWatched(){
+    //This value should be replaced by props
     const [watched, setWatched] = useState(false);
     const [hoverWatched, setHoverWatched] = useState(false);
 
     return(
+
+        //Create a function to change value of watched when the user click the component
+        //Add validation and restriction with StarRating
+        
         <div className="film-watched-component">
             <label>
                 <input

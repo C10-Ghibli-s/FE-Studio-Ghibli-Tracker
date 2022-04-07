17d8dea2f240cc43bf3e7cfa42d9ea4ee4783c98
Ivan Garcia <ivangm180@gmail.com>
2022-04-07 01:40:24 -0500

    [ADD] creating forms, editing facebook button and making tests

--- src/pages/Register/Register.test.js
+++ src/pages/Register/Register.test.js
@@ -7,14 +7,6 @@ import "@testing-library/jest-dom/extend-expect";
 import React from "react";
 import { Register } from "../Register";
 
-it("should give a user and password field", () => {
-  render(<Register />);
-  const user = screen.getByText(/user/i);
-  const password = screen.getByText(/password/i);
-  expect(user).toBe("User");
-  expect(password).toBe("Password");
-});
-
 it("should render a button", () => {
   render(<Register />);
   const registerButton = screen.getByText(/register/i);

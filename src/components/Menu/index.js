92fcef54bc59bcdc45904a1099eccfbcf93e86f6
Hai5e <haisedfm@hai5e.com>
2022-04-07 13:40:39 -0500

    finishing CSS profile

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,18 +1,14 @@
-import React from "react";
-import { useState } from 'react';
+import React, { useState } from "react";
 import openMenuButton from './images/openMenuButton.png';
 import './menu.scss';
 import { NavLinks } from "./NavLinks";
 import { motion } from 'framer-motion';
 
-function Menu() {
-    const [menuOpen, setMenuOpen] = useState(false);
-    //nav-menu links - USED IN THE TEST / EVERY CHANGE IN HERE, MUST BE IN THE TEST FILE
+function Menu({menuOpen,setMenuOpen, toggleFilter, setToggleFilter}) {
     const links = [
         { pageRoute: '/home', page : 'home'}, 
         { pageRoute: '/profile', page: 'profile'},
-        { pageRoute: '/scores', page: 'scores'}
-        ];
+    ];
     return(
         <React.Fragment>
             <div data-testid="menu-Navbar" className="menuContainer">
@@ -21,7 +17,11 @@ function Menu() {
                     animate={{opacity : 1}}
                     className="menu__openMenuButton"
                     type="button"
-                    onClick={() => setMenuOpen(!menuOpen)}>
+                    onClick={() => {setMenuOpen(!menuOpen);
+                        if(toggleFilter) {
+                            setToggleFilter(!toggleFilter);
+                        };
+                    }}>
                     <img src={openMenuButton}></img>
                 </motion.button>
                 <nav>
